009e5b73c33ea6c7baed83fc2091403467cf3b16
Pranav Sinha <pranavsinha922@gmail.com>
2023-08-27 17:00:45 +0530

    Update main.rs

--- 2. Common Programming Concepts/2.2 Mutability/src/main.rs
+++ 2. Common Programming Concepts/2.2 Mutability/src/main.rs
@@ -6,4 +6,9 @@ fn main() {
     another_number = 16;
 
     println("{}", another_number);
+
+    let num = 16;
+    let num = num + 1;     // though the variable is immutable we can shadow the variable by re-declaring it
+
+    println!("{}", num);
 }
