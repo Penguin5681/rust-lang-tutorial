67451d1ee856a72c708df2d52e17a37f69ac8535
Pranav Sinha <pranavsinha922@gmail.com>
2023-11-01 20:30:33 +0530

    Update main.rs

--- 5. Enums and Pattern Matching/5.3 Concise Control Flow with if let/src/main.rs
+++ 5. Enums and Pattern Matching/5.3 Concise Control Flow with if let/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    let some_value: Option<i32> = Some(42);
+
+    if let Some(value) = some_value {
+        println!("Found a value: {}", value);
+    } else {
+        println!("No value found.");
+    }
 }
+
+// You'll find some other examples in the readme.md file
