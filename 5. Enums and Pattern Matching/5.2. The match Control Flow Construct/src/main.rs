3b5460444dc87ba013ce997f9ca128820388125b
Pranav Sinha <pranavsinha922@gmail.com>
2023-11-01 20:19:26 +0530

    Update main.rs

--- 5. Enums and Pattern Matching/5.2. The match Control Flow Construct/src/main.rs
+++ 5. Enums and Pattern Matching/5.2. The match Control Flow Construct/src/main.rs
@@ -12,12 +12,9 @@ enum Coins {
 
 fn value_in_cents(coin: Coins) -> u8 {
     match coin {
-        Coins::Penny => {
-            println!("Lucky penny!");
-            1
-        }
-        Coins::Nickel => 5,
         Coins::Dime => 10,
         Coins::Quarter => 25,
+        Coins::Penny => 1,
+        Coins::Nickel => 5,
     }
 }
